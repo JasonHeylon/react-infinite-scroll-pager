1c5473f733decfc3eb7821ee9e8e60c6271d13c2
Jason Heylon <jasonheylon@gmail.com>
2020-04-01 14:42:24 +0800

    Updates

---
+++ precache-manifest.042f20792dbec65e3ad8113412025537.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc3920e5cbb075a395c99701b07caee7",
+    "url": "/react-infinite-scroll-pager/index.html"
+  },
+  {
+    "revision": "9cd87d9ba2a4aa122056",
+    "url": "/react-infinite-scroll-pager/static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "b8a320e728c3e715ce9f",
+    "url": "/react-infinite-scroll-pager/static/js/2.065d7047.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-infinite-scroll-pager/static/js/2.065d7047.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9cd87d9ba2a4aa122056",
+    "url": "/react-infinite-scroll-pager/static/js/main.09018050.chunk.js"
+  },
+  {
+    "revision": "9698758fd3742ba7bcef",
+    "url": "/react-infinite-scroll-pager/static/js/runtime-main.3db4d524.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/react-infinite-scroll-pager/precache-manifest.042f20792dbec65e3ad8113412025537.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-infinite-scroll-pager/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
